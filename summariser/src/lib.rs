513c182c42cfb12e001a2d141c1dacd99c4c21cb
Fan Zeng <fanzengau@gmail.com>
2024-05-30 23:50:26 +1000

    Update to take section length as arg

--- summariser/src/lib.rs
+++ summariser/src/lib.rs
@@ -1,43 +1,33 @@
-use std::env;
 use std::error::Error;
 use std::fs;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
-    let results = if config.ignore_case {
-        search_case_insensitive(&config.query, &contents)
-    } else {
-        search(&config.query, &contents)
-    };
-    for line in results {
+    for line in contents.lines() {
         println!("{line}");
     }
     Ok(())
 }
 
 pub struct Config {
-    pub query: String,
     pub file_path: String,
-    pub ignore_case: bool,
+    pub section_len: i32,
 }
 
 impl Config {
     pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
         args.next();
-
-        let query = match args.next() {
-            Some(arg) => arg,
-            None => return Err("Didn't get a query string"),
-        };
         let file_path = match args.next() {
             Some(arg) => arg,
             None => return Err("Didn't get a file path"),
         };
-        let ignore_case = env::var("IGNORE_CASE").is_ok();
+        let section_len: i32 = match args.next() {
+            Some(arg) => arg.parse().expect("Error parsing section_len"),
+            None => return Err("Didn't get a section length"),
+        };
         Ok(Config {
-            query,
             file_path,
-            ignore_case,
+            section_len,
         })
     }
 }
@@ -64,28 +54,28 @@ pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a st
 mod tests {
     use super::*;
 
-    #[test]
-    fn case_sensitive() {
-        let query = "duct";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Duct tape.";
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
-    }
+    // #[test]
+    //     fn case_sensitive() {
+    //         let query = "duct";
+    //         let contents = "\
+    // Rust:
+    // safe, fast, productive.
+    // Pick three.
+    // Duct tape.";
+    //         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    //     }
 
-    #[test]
-    fn case_insensitive() {
-        let query = "rUsT";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Trust me.";
-        assert_eq!(
-            vec!["Rust:", "Trust me."],
-            search_case_insensitive(query, contents)
-        );
-    }
+    // #[test]
+    //     fn case_insensitive() {
+    //         let query = "rUsT";
+    //         let contents = "\
+    // Rust:
+    // safe, fast, productive.
+    // Pick three.
+    // Trust me.";
+    //         assert_eq!(
+    //             vec!["Rust:", "Trust me."],
+    //             search_case_insensitive(query, contents)
+    //         );
+    //     }
 }
